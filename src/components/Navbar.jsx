84feadeec272a1de07ce3b8f89925d885fa8782c
Anish Kumar <anishkumargvm02@gmail.com>
2025-01-16 12:16:22 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
+import { FaXTwitter } from "react-icons/fa6";
+
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav className='mb-20 flex items-center justify-between py-6'>
+            <div className='flex flex-shrink-0 items-center'>
+                <h2 className='text-3xl'>Ak</h2>
+            </div>
+            <div className='m-8 flex items-center justify-center gap-4 '>
+                <a href="https://github.com/anis5165" target='_blank'><FaGithub/></a>
+                <a href="https://www.linkedin.com/in/anish-kumar-verma-4007a6255/" target='_blank'><FaLinkedin/></a>
+                <FaInstagram/>
+                <a href="https://x.com/anish_5165" target='_blank'><FaXTwitter/></a>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/constants/index.js
@@ -0,0 +1,72 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EDUCATIONS = [
+  {
+    year: "2024 - Present",
+    degree: "M.Sc Computer Science",
+    place: "Dr. Rammanohar Lohia Avadh University, Ayodhya(U.P.)",
+    description: `Pursuing an MSc in Computer Science from Dr. Rammanohar Lohia Avadh University provides advanced knowledge, research opportunities, and industry readiness.`,
+  },
+  {
+    year: "2021 - 2024",
+    degree: "B.Sc Computer Science",
+    place: "Dr. Rammanohar Lohia Avadh University, Ayodhya(U.P.)",
+    description: `Graduated with a BSc in Computer Science from Dr. Rammanohar Lohia Avadh University provides a strong foundation in computing, programming, and problem-solving.`,
+  },
+  {
+    year: "2020 - 2021",
+    degree: "10+2 Non-Medical",
+    place: "Gopal Vidya Mandir Sr. Sec. School, Jind(Hr.)",
+    description: `Passed +2 (Non-Medical) from Gopal Vidya Mandir Sr. Sec. School with 81% marks, excelling in mathematics, physics, and chemistry.`,
+  },
+  {
+    year: "2018 - 2019",
+    degree: "10th",
+    place: "Gopal Vidya Mandir Sr. Sec. School, Jind(Hr.)",
+    description: `Passed 10th from Gopal Vidya Mandir Sr. Sec. School with 76.6% marks, showcasing strong academic performance across all subjects.`,
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Code Collab Website",
+    image: project1,
+    description:
+      "Open-Source Repo Finder,Your go-to platform for discovering open-source projects tailored to your skills and interests!",
+    technologies: ["NEXTJS", "Tailwind CSS", "ExpressJS", "Node.js", "MongoDB"],
+  },
+  {
+    title: "AI Galaxy Plugin",
+    image: project2,
+    description:
+      "Documentation for AI Extensions! This guide will help you integrate our AI components into your own projects seamlessly. Our components are designed to be plug-and-play, requiring minimal setup.",
+      technologies: ["NEXTJS", "Tailwind CSS", "ExpressJS", "Node.js", "MongoDB"],
+    },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["ReactJS", "Tailwind CSS", "Framer Motion"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Ajmer Basti Jind, Haryana 126102",
+  phoneNo: "+91 97297 26784",
+  email: "anishkumargvm02@gmail.com",
+};
